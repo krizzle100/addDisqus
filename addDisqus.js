d43f7447a52d9548f366aae98e4fd6504c959827
Kris <34043455+krizzle100@users.noreply.github.com>
2020-03-31 16:32:09 -0400

    Update addDisqus.js

--- addDisqus.js
+++ addDisqus.js
@@ -5,22 +5,22 @@ answersSdk.addListener(answersSdk.events.pageLoad, function (event) {
   if (answersSdk.getVisiblePageType() === answersSdk.pages.article) {
   
   //Disqus config vars for article-specific comments.
-  var disqus_config = function () { 
+  const disqus_config = function () { 
   this.page.url = answersSdk.getVisiblePageData().article.url;
   this.page.identifier = answersSdk.getVisiblePageData().article.id;
   };
   
   //Targets DOM node after where we will insert Disqus container.
-  let targetNode = document.querySelector(".article-actions");
+  const targetNode = document.querySelector(".article-actions");
     
   //Creates DIV where Disqus container will reside.
-  let createDisqusContainer = document.createElement('div')
+  const createDisqusContainer = document.createElement('div')
   createDisqusContainer.setAttribute("id", "disqus_thread")
   targetNode.appendChild(createDisqusContainer)
 
   //Loads Disqus script.
-  let selectDisqusContainer = document.querySelector("#disqus_thread")
-  let insertDisqus = document.createElement('script')
+  const selectDisqusContainer = document.querySelector("#disqus_thread")
+  const insertDisqus = document.createElement('script')
   insertDisqus.src = ''; //Add account specific Disqus script URL e.g. https://<YOUR_DOMAIN>.disqus.com/embed.js
   insertDisqus.setAttribute('data-timestamp', +new Date());
   targetNode.appendChild(insertDisqus)
